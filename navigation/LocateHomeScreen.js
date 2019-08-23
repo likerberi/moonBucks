6d24c5816261a0b024b15f532c604e3a6c604abb
stepbystep <likerberi97@gmail.com>
2019-08-23 19:10:34 +0900

    native is not compatible with material...

--- navigation/LocateHomeScreen.js
+++ navigation/LocateHomeScreen.js
@@ -1,33 +1,29 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
-import { Box } from '@material-ui/core/Box'; 
-import { palette } from '@material-ui/system';
-import { Badge, ListItem, Button } from 'react-native-elements';
-
+import React, { Component } from 'react';
+import { Text, StyleSheet, View, Button } from 'react-native';
+import { Card } from 'react-native-elements';
 export default class LocateHomeScreen extends Component {
     
     static navigationOptions = {
         title: "This is LocateHomeScreen. Share/Now/Detail"
     }
     
-    render() {
-        return (
-            <View>
-                <Box bgcolor="text.secondary" color="primary.main">
-                    <Button>Sample Message</Button>
-                </Box>
-            </View>
-        )
+    pleaseChange() {
+        alert("change music");
     }
-}
 
-const styles = StyleSheet.create({
-    munan: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    badgeFont: {
-        fontSize: 12,
-    }
-})
+    render() {
+        return (            
+                <View>
+                    <Card
+                        title="좌석 공유 하기"
+                    />
+                    <Card
+                        title="입찰 현황 보기"
+                    />
+                    <Card
+                        title="정산 관련 조회"
+                    />
+                </View>
+        )
+    };
+};
